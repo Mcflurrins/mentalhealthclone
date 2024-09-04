b0415655cd9309f51c9ccce94a7c9e5cce054037
Mcflurrins <flori.andrea.60@gmail.com>
2024-09-04 10:44:15 +0700

    tutorial 1 update

--- main/views.py
+++ main/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'npm' : '2306123456',
-        'name': 'Pak Bepe',
-        'class': 'PBP E'
+        'npm' : '2306171713',
+        'name': 'Flori Andrea Ng',
+        'class': 'PBP KKI'
     }
 
     return render(request, "main.html", context)
