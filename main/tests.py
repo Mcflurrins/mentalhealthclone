13e72ad6e400ec7343e5443857fd0f1b0cd54ae9
Mcflurrins <flori.andrea.60@gmail.com>
2024-09-20 21:47:11 +0700

    first push

--- main/tests.py
+++ main/tests.py
@@ -15,12 +15,7 @@ class MainTest(TestCase):
         response = Client().get('/skibidi/')
         self.assertEqual(response.status_code, 404)
 
-    def test_strong_mood_user(self):
-        now = timezone.now()
-        mood = MoodEntry.objects.create(
-          mood="Happy",
-          time = now,
-          feelings = "I'm happy, even though my clothes are soaked from the rain :(",
-          mood_intensity = 8,
-        )
-        self.assertTrue(mood.is_mood_strong)
+    def test_main_template_uses_correct_page_title(self):
+        response = Client().get("/")
+        html_response = response.content.decode("utf8")
+        self.assertIn("PBD Mental Health Tracker", html_response)
